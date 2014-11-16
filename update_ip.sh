8d400f8f383f3e7e2a54751d3800092478c0b3f6
needle <needlewang2011@gmail.com>
2014-11-16 18:10:07 +0800

    get web ip shell

--- update_ip.sh
+++ update_ip.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -
-#
+#the work dir should is git workspace
 #2014年 11月 16日 星期日 17:47:28 CST
+
+PATH='/usr/lib/lightdm/lightdm:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/jdk1.8.0_20/bin:/home/needle/myshell'
+export PATH
+
+cd /home/needle/test/gittopush/temp_note || {
+echo 'can not into temp_note dir~'
+exit 1
+}
 
 ip_file="./tempfile"
 getipshell="./getmywebip.sh"
@@ -15,8 +23,14 @@ new_ip=$($getipshell)
 #echo -e "old ip is $old_ip\nnew ip is $new_ip"
 
 if [[ "${old_ip}" != "${new_ip}" ]]; then
-    echo "$new_ip" > "$ip_file"
     #echo "It's different. now update the web ip."
+    if [[ -w "$ip_file" ]]; then
+        echo "$new_ip" > "$ip_file"
+        git push
+    else
+        echo "can not write new ip into $ip_file"
+        :
+    fi
 else
     #echo "the web ip is the same."
     :
